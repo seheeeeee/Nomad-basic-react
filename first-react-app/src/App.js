349d948af7ab9d604475b74f99e91b4cd1b9dcdd
LeeSeHee <euc728@naver.com>
2022-04-06 23:24:56 +0900

    1. useEffect(함수, []) => 처음 렌더링 될 때 한 번만 2. useEffect(함수, [keyword]) => 처음 렌더링 될 때 + keyword가 변할 때 3. useEffect(함수, [keyword, counter]) => 처음 렌더링 될 때 + keyword & counter 가 변할 때

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -2,13 +2,30 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onCLick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
   useEffect(() => {
-    console.log("CALL THE API ....");
+    console.log("I run only once");
   }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'counter' and 'keyword' changes");
+  }, [counter, keyword]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
       <h1>{counter}</h1>
       <button onClick={onCLick}>click</button>
     </div>
