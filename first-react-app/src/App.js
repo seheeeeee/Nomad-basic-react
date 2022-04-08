285905d58424147628ebbed570b4e8207d0d4e5e
LeeSeHee <euc728@naver.com>
2022-04-08 21:53:43 +0900

    [unsolved] coinconverter 만드는 중

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,36 +1,36 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setTodo] = useState("");
-  const [toDos, setTodos] = useState([]);
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setTodos((currentArray) => [toDo, ...currentArray]);
-    setTodo("");
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [budget, setBudget] = useState(0);
+  const [option, setOptions] = useState(0);
+  const onChange = (event) => setBudget(event.target.value);
+  const selectedCoin = (event) => {
+    setOptions(event.target.value);
   };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
+      <h1>the Coins! ({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <p>현재 예산을 적어주세요(USD 기준)</p>
+      <input onChange={onChange} value={budget} type="text" />
+      <p>어떤 코인을 구매하기 원하시나요?</p>
+      <select onChange={selectedCoin}>
+        {coins.map((coin, index) => (
+          <option key={index}>{coin.name}</option>
         ))}
-      </ul>
+      </select>
+      <p>당신이 살 수 있는 코인은 : {option > 0 ? budget / option : null}</p>
     </div>
   );
 }
