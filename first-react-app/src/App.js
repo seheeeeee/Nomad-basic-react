139898289e8e30f5b9d3d83adf1bdde41eed0e07
LeeSeHee <euc728@naver.com>
2022-04-09 11:58:33 +0900

    라우터 연결과 Link 사용법**

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/movie" element={<Detail />}></Route>
         <Route path="/" element={<Home />}></Route>
       </Routes>
     </Router>
