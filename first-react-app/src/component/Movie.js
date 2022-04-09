fb09ab78f67605ec2b77751fad6067a1730a1ac7
LeeSeHee <euc728@naver.com>
2022-04-09 13:19:48 +0900

    route link에 params로 id 전달해서 detail componet 생성

--- first-react-app/src/component/Movie.js
+++ first-react-app/src/component/Movie.js
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ coverImg, title, summary, genres, id }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
       <h2>
-        <Link to="/movie">{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
@@ -23,5 +23,6 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+  id: PropTypes.number.isRequired,
 };
 export default Movie;
--- first-react-app/src/routes/Detail.js
+++ first-react-app/src/routes/Detail.js
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovies] = useState([]);
+  const { id } = useParams(); //params 정보 가져오기
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovies(json.data.movie);
+    setLoading(false);
+    console.log(movie);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h1>{movie.title}</h1>
+          <img src={movie.medium_cover_image} alt={movie.title} />
+          <p>{movie.description_full}</p>
+          <ul>
+            {movie.genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
