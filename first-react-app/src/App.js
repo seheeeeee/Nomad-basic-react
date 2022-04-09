19968a078ea3e8a5791709732af79cf0cf168515
LeeSeHee <euc728@naver.com>
2022-04-09 10:30:18 +0900

    map 으로 loop 돌려서 리스트 만들기**

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -3,34 +3,29 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [budget, setBudget] = useState(0);
-  const [option, setOptions] = useState(0);
-  const onChange = (event) => setBudget(event.target.value);
-  const selectedCoin = (event) => {
-    setOptions(event.target.value);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
   return (
     <div>
-      <h1>the Coins! ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <p>현재 예산을 적어주세요(USD 기준)</p>
-      <input onChange={onChange} value={budget} type="text" />
-      <p>어떤 코인을 구매하기 원하시나요?</p>
-      <select onChange={selectedCoin}>
-        {coins.map((coin, index) => (
-          <option key={index}>{coin.name}</option>
-        ))}
-      </select>
-      <p>당신이 살 수 있는 코인은 : {option > 0 ? budget / option : null}</p>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>{movie.title}</div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
